c1d001b5b5cdaf3b5a49b8f3c2bee70d9ac00fff
Taph-tech <mustaphac687@gmail.com>
2025-07-30 14:25:51 +0000

    return str.chars().anyMatch(c -> Character.isUpperCase(c) ||                 Character.isLowerCase(c)|| Character.isDigit(c))

--- 01/01_02/start/Algorithms/src/Algorithms.java
+++ 01/01_02/start/Algorithms/src/Algorithms.java
@@ -1,6 +1,17 @@
 public class Algorithms {
 
+
     public static void main(String[] args) {
 
+    }
+
+    public static boolean isPassWordComplex(String str){
+        return str.chars().anyMatch(Character::isUpperCase) && 
+        str.chars().anyMatch(Character::isLowerCase) && 
+        str.chars().anyMatch(Character::isDigit);
+
+        //Or the below can be used
+       /*  return str.chars().anyMatch(c -> Character.isUpperCase(c) ||
+                Character.isLowerCase(c)|| Character.isDigit(c));*/
     }
 }
