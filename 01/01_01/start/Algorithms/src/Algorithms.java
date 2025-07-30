3b4809e61f82693d451157b05c933d37dfdfe588
Taph-tech <mustaphac687@gmail.com>
2025-07-30 13:01:44 +0000

    start file added with functions isUppercase & isLowercase

--- 01/01_01/start/Algorithms/src/Algorithms.java
+++ 01/01_01/start/Algorithms/src/Algorithms.java
@@ -1,6 +1,21 @@
 public class Algorithms {
 
+
+    public static boolean isUppercase(String str)
+    {
+        return str.chars().noneMatch(Character::isUpperCase);
+        //return str.chars().allMatch(Character::isUpperCase);
+    }
+
+     public static boolean isLowercase(String str)
+    {
+        return str.chars().allMatch(Character::isUpperCase);
+    }
     public static void main(String[] args) {
+
+        System.out.println(isUppercase("Hello"));
+        System.out.println(isUppercase("HELLO"));
+        System.out.println(isUppercase("hello"));
 
     }
 }
