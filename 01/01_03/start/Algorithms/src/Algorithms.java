c56bdf27040039c17ee8670083651879f5a2af5d
Taph-tech <mustaphac687@gmail.com>
2025-07-31 13:04:55 +0000

    normalizeString(String s) method added to trim and convert text to lowercase

--- 01/01_03/start/Algorithms/src/Algorithms.java
+++ 01/01_03/start/Algorithms/src/Algorithms.java
@@ -1,6 +1,16 @@
 public class Algorithms {
+     
+    public static String normalizeString(String s)
+    {
+        return s.toLowerCase().trim().replace(",","");
+        // String lowerCase = s.toLowerCase();
+        // String trimmed = lowerCase.trim();
+        // String normalized = trimmed.replace(",","");
+        // return normalized;
+    }
 
     public static void main(String[] args) {
+        System.out.println(normalizeString("  Hello, There Buddy!  "));
         
     }
 }
