b854aa21bd93b179bf711f8ca385aaaad9ebc9f3
Taph-tech <mustaphac687@gmail.com>
2025-08-01 13:21:38 +0000

    parseContents(String c) function added

--- 01/01_04/start/Algorithms/src/Algorithms.java
+++ 01/01_04/start/Algorithms/src/Algorithms.java
@@ -1,6 +1,20 @@
 public class Algorithms {
 
+    public static void parseContents(String c){
+        System.out.println("Option 1");
+        for (char s : c.toCharArray() ){
+            System.out.print(s);
+        }
+
+
+        System.out.println("\nOption 2");
+        for (int i =0; i<c.length(); i++){
+            System.out.print(c.charAt(i));
+        }
+    }
     public static void main(String[] args) {
         String s = "HeLLo";
+        System.out.println(s.contains("LL"));
+        parseContents(s);
     }
 }
