89d40de4ca1da82dc787f728d015d555501a096d
Taph-tech <mustaphac687@gmail.com>
2025-08-01 14:36:12 +0000

    Folder 01_05 in start method isAtEvenIndex added

--- 01/01_05/start/Algorithms/src/Algorithms.java
+++ 01/01_05/start/Algorithms/src/Algorithms.java
@@ -1,10 +1,25 @@
 public class Algorithms {
 
     public static boolean isAtEvenIndex(String s, char item) {
+        if( s== null || s.isEmpty()){
+            return false;
+        }
+        for (int i = 0; i < s.length()/2+1; i += 2) {
+            if (s.charAt(i) == item) {
+                return true;
+            }
+        }
         return false;
     }
 
     public static void main(String[] args) {
+        // Test cases
+        System.out.println(isAtEvenIndex("hello", 'h')); // true
+        System.out.println(isAtEvenIndex("hello", 'e')); // false
+        System.out.println(isAtEvenIndex("hello", 'l')); // true
+        System.out.println(isAtEvenIndex("hello", 'o')); // false
+        System.out.println(isAtEvenIndex("", 'a')); // false
+        System.out.println(isAtEvenIndex(null, 'a')); // false
 
     }
 }
