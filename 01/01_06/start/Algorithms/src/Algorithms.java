1ce6816c0c0d00dfc200f6ec2ffa888438da092a
Taph-tech <mustaphac687@gmail.com>
2025-08-17 15:18:31 +0000

    01_05, 01_06, 01_07 start files updated with methods

--- 01/01_06/start/Algorithms/src/Algorithms.java
+++ 01/01_06/start/Algorithms/src/Algorithms.java
@@ -1,6 +1,28 @@
 public class Algorithms {
 
-    public static void main(String[] args) {
 
+    public static String reverString(String s)
+    {
+        if(s==null || s.length() == 0)
+        {
+            return s;
+        }
+        StringBuilder reversed = new StringBuilder();
+
+        for( int i= s.length() -1; i >= 0; i--)
+        {
+            reversed.append(s.charAt(i));
+        }
+
+        return reversed.toString();
+    }
+    public static void main(String[] args) {
+        String s = "Hello My Friend";
+        System.out.println(reverString(s));
+        System.out.println(reverString("Java Programming"));
+        System.out.println(reverString("Algorithms"));
+        System.out.println(reverString("")); // Test with empty string
+        System.out.println(reverString(null)); // Test with null input
+        System.out.println(reverString("A")); // Test with single character
     }
 }
