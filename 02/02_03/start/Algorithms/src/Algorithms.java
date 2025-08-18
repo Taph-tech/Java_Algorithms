d7c1c8460386881cce2a79ec5f372f7a626f11e3
Mustapha Colley <mcolley@agib.gm>
2025-08-18 17:13:08 +0000

    BinarySearch implimented in folder 02_03_start

--- 02/02_03/start/Algorithms/src/Algorithms.java
+++ 02/02_03/start/Algorithms/src/Algorithms.java
@@ -1,6 +1,25 @@
 public class Algorithms {
 
     public static boolean binarySearch(int[] arr, int item) {
+        int min = 0;
+        int max = arr.length -1;
+
+        while(min <= max)
+        {
+            int mid = (min + max) / 2;
+            if(item == arr[mid])
+            {
+                return true;
+            }
+            else if(item < arr[mid])
+            {
+                max = mid - 1;
+            }
+            else
+            {
+                min = mid + 1;
+            }
+        }
         return false;
     }
 
