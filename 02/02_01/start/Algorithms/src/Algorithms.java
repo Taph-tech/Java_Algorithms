3f95718775073f51be7397436e0df82687b423a8
Mustapha Colley <mcolley@agib.gm>
2025-08-18 15:56:41 +0000

    Chap 1, 06-09 added and Chap 2, 01-02 added

--- 02/02_01/start/Algorithms/src/Algorithms.java
+++ 02/02_01/start/Algorithms/src/Algorithms.java
@@ -1,12 +1,20 @@
 public class Algorithms {
 
-    public static boolean linearSearch(int[] arr, int item) {
-        return false;
+    public static Integer linearSearch(int[] arr, int item) {
+        for(int current: arr)
+        {
+            if (current == item){
+                return item;
+            }
+            
+        }
+                return null;
     }
 
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3, 4, 5, 6 };
         System.out.println(linearSearch(arr, 1));
         System.out.println(linearSearch(arr, 8));
+        System.out.println(linearSearch(arr, 3));
     }
 }
