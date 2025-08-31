32f8ef39b0a094a8793683ae159d3e93709bdd82
Taph-tech <104026858+Taph-tech@users.noreply.github.com>
2025-08-31 10:42:47 +0000

    ReverseInplace method created...

--- 02/02_05/start/Algorithms/src/Algorithms.java
+++ 02/02_05/start/Algorithms/src/Algorithms.java
@@ -10,11 +10,20 @@ public class Algorithms {
         return result;
     }
 
-
+  public static void reverseInplace(int [] arr){
+    for (int i= 0; i< arr.length/2; i++) {
+        int temp = arr[i];
+        arr[i] = arr[arr.length - i - 1];
+        arr[arr.length - i - 1] = temp;
+    }
+  }
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3, 4, 5, 6 };
 
-        Arrays.stream(reverse(arr))
-                .forEach(System.out::println);
+       /* Arrays.stream(reverse(arr))
+                .forEach(System.out::println); */
+                reverseInplace(arr);
+                reverseInplace(arr);
+                Arrays.stream(arr).forEach(System.out::println);
     }
 }
